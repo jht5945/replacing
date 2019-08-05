c9fff1d3ba2ee037e08440e4dcd5a6defaa12c3b
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-05 23:56:36 +0800

    use get_term_width_message

--- src/main.rs
+++ src/main.rs
@@ -13,6 +13,43 @@ use std::{
 };
 
 
+pub fn get_term_width() -> Option<usize> {
+    match term_size::dimensions() {
+        None => None,
+        Some((w, _h)) => Some(w),
+    }
+}
+
+// thanks https://blog.csdn.net/star_xiong/article/details/89401149
+pub fn find_char_boundary(s: &str, index: usize) -> usize {
+    if s.len() <= index {
+        return index;
+    }
+    let mut new_index = index;
+    while !s.is_char_boundary(new_index) {
+        new_index += 1;
+    }
+    new_index
+}
+
+pub fn get_term_width_message(message: &str, left: usize) -> String {
+    match get_term_width() {
+        None => message.to_string(),
+        Some(w) => {
+            let len = message.len();
+            if w > len {
+               return message.to_string();
+            }
+            let mut s = String::new();
+            s.push_str(&message[0..find_char_boundary(&message, w-10-5-left)]);
+            s.push_str("[...]");
+            s.push_str(&message[find_char_boundary(&message, len-10)..]);
+            s
+        },
+    }
+}
+
+
 struct Options {
     version: bool,
     verbose: bool,
@@ -146,7 +183,7 @@ fn replace_files(options: &Options, dir_path: &Path) -> XResult<()> {
                 //    }
                 //    return false;
                 //}
-                print_lastline(&format!("Scanning: {}", p_str));
+                print_lastline(&get_term_width_message(&format!("Scanning: {}", p_str), 10))
             },
         }
         true
