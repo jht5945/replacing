5f3aedca2c82b0e55c0b6af43a5a1c3452316928
Hatter Jiang@Pixelbook <jht5945@gmail.com>
2019-08-06 08:34:42 +0800

    update rust_util lib

--- src/main.rs
+++ src/main.rs
@@ -14,42 +14,6 @@ use std::{
 };
 
 
-pub fn get_term_width() -> Option<usize> {
-    match term_size::dimensions() {
-        None => None,
-        Some((w, _h)) => Some(w),
-    }
-}
-
-// thanks https://blog.csdn.net/star_xiong/article/details/89401149
-pub fn find_char_boundary(s: &str, index: usize) -> usize {
-    if s.len() <= index {
-        return index;
-    }
-    let mut new_index = index;
-    while !s.is_char_boundary(new_index) {
-        new_index += 1;
-    }
-    new_index
-}
-
-pub fn get_term_width_message(message: &str, left: usize) -> String {
-    match get_term_width() {
-        None => message.to_string(),
-        Some(w) => {
-            let len = message.len();
-            if w > len {
-               return message.to_string();
-            }
-            let mut s = String::new();
-            s.push_str(&message[0..find_char_boundary(&message, w-10-5-left)]);
-            s.push_str("[...]");
-            s.push_str(&message[find_char_boundary(&message, len-10)..]);
-            s
-        },
-    }
-}
-
 pub fn read_file_content(file: &Path, large_file_len: u64) -> XResult<String> {
     if ! file.exists() {
         return Err(new_box_error(&format!("File not exists: {:?}", file)));
